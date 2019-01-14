d4f070a02de4a1cb1d560587fc9607026086f052
fangyu <wb-fangyu@zhongan.io>
2019-01-14 11:18:37 +0800

    aaa

--- pel/src/components/Reg.vue
+++ pel/src/components/Reg.vue
@@ -5,35 +5,50 @@
     </userHead>
     <form action="#" method="get" class="reg-login">
       <!-- 无接口本地储存模拟 -->
-      <h3>已经有账号了？点此<router-link class="orange" :to = "{name:'login'}">登录</router-link></h3>
+      <h3>
+        已经有账号了？点此
+        <router-link class="orange" :to="{name:'login'}">登录</router-link>
+      </h3>
       <div class="lrBox">
-       <div class="lrList2"><input type="text" placeholder="起个可爱的昵称" v-model = "nikeName"/> </div>
-       <div class="lrList"><input type="text" placeholder="输入手机号码或者邮箱号------/^\d/" v-model = "tel"/>{{tel}}</div>
-       <div class="lrList"><input type="text" placeholder="设置新密码（6-18位数字或字母" v-model = "pass" /></div>
-       <div class="lrList"><input type="text" placeholder="再次输入密码" v-model = "passAgain"/></div>
-      </div><!--lrBox/-->
-      <div class="lrSub">
-       <input type="button" value="立即注册" @click = "reg" />
+        <div class="lrList2">
+          <input type="text" placeholder="起个可爱的昵称" v-model="nikeName">
+        </div>
+        <div class="lrList">
+          <input type="text" placeholder="输入手机号码或者邮箱号------/^\d/" v-model="tel">
+          {{tel}}
+        </div>
+        <div class="lrList">
+          <input type="text" placeholder="设置新密码（6-18位数字或字母" v-model="pass">
+        </div>
+        <div class="lrList">
+          <input type="text" placeholder="再次输入密码" v-model="passAgain">
+        </div>
       </div>
-     </form><!--reg-login/-->
-     <div class="height1"></div>
+      <!--lrBox/-->
+      <div class="lrSub">
+        <input type="button" value="立即注册" @click="reg">
+      </div>
+    </form>
+    <!--reg-login/-->
+    <div class="height1"></div>
   </div>
 </template>
 
 <script>
 import userHead from "./userHead.vue";
-import store from './../store.js'
+import store from "./../store.js";
 export default {
   name: "reg",
   data() {
     return {
-      userInfo:[],
+      userInfo: [],
       nikeName: "",
       tel: "",
       pass: "",
       passAgain: "",
       userInfo: {},
-      errorBorder: false
+      errorBorder: false,
+      day: 0, hr: 0, min: 0, sec: 0
     };
   },
   components: {
@@ -42,18 +57,17 @@ export default {
   methods: {
     reg() {
       let userInfo = eval(localStorage.getItem("fang"));
-        let newId = {};
+      let newId = {};
       if (/^\d/.test(this.tel) && this.pass == this.passAgain) {
         for (let i = 0; i < userInfo.length; i++) {
-          if (userInfo[i].tel == this.tel ) {
+          if (userInfo[i].tel == this.tel) {
             // alert("老铁，你来晚了，换个号试试");
-            break
-          }
-           else{
+            break;
+          } else {
             newId.tel = this.tel;
             newId.nikeName = this.nikeName;
             newId.pass = this.pass;
-            newId.car = [{pro:{},count:0}];
+            newId.car = [{ pro: {}, count: 0 }];
             userInfo.push(newId);
             localStorage.setItem("fang", JSON.stringify(userInfo));
             this.$router.replace({ name: "login" });
@@ -62,8 +76,28 @@ export default {
       } else {
         confirm("你是不是哪嘎错了？");
       }
+    },
+    countdown() {
+      const end = Date.parse(new Date("2017-12-01"));
+      const now = Date.parse(new Date());
+      const msec = end - now;
+      let day = parseInt(msec / 1000 / 60 / 60 / 24);
+      let hr = parseInt((msec / 1000 / 60 / 60) % 24);
+      let min = parseInt((msec / 1000 / 60) % 60);
+      let sec = parseInt((msec / 1000) % 60);
+      this.day = day;
+      this.hr = hr > 9 ? hr : "0" + hr;
+      this.min = min > 9 ? min : "0" + min;
+      this.sec = sec > 9 ? sec : "0" + sec;
+      const that = this;
+      setTimeout(function() {
+        that.countdown();
+      }, 1000);
     }
   },
+  mounted(){
+    this.countdown()
+  }
 };
 </script>
 
